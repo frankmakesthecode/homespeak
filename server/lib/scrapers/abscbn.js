53e1ab088e32da3311e15c6c6770f5ba970fbcf6
Francis Yu <francisjosephyu@gmail.com>
2022-02-22 20:03:26 +0800

    Testing: Something new

--- server/lib/scrapers/abscbn.js
+++ server/lib/scrapers/abscbn.js
@@ -1,21 +1,13 @@
 const puppeteer = require('puppeteer');
 const handleDuplicates = require('../handleDuplicates');
 
-const scrapeAbscbn = async () => {
+const scrapeManilaTimes = async () => {
   try {
     const chromeOptions = {
       headless: true,
-      defaultViewport: null,
-      args: [
-        '--incognito',
-        '--no-zygote',
-        '--single-process',
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-      ],
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
     };
-
-    const url = 'https://news.abs-cbn.com/anc/exclusives';
+    const url = 'https://www.manilatimes.net/news';
     const browser = await puppeteer.launch(chromeOptions);
     const page = await browser.newPage();
     await page.setUserAgent(
@@ -27,48 +19,115 @@ const scrapeAbscbn = async () => {
       timeout: 0,
     });
 
-    await page.screenshot({ fullPage: true, path: 'screenshot.png' });
-    // Grab news headlines
     const results = await page.evaluate(() => {
-      const elem = document.querySelector('a.section-top1-title-link');
+      const elem = document.querySelector('div.article-title-h1 > a');
+
       const headline = {};
       headline.sourceId = 3;
-      headline.title = elem.innerHTML;
+      headline.title = elem.innerText;
       headline.link = elem.href;
 
       const latest = Array.from(
-        document.querySelectorAll('a.s-title-small-link')
+        document.querySelectorAll('div.article-title-h3 > a')
       ).map((elem) => {
         const data = {};
 
         data.sourceId = 3;
-        data.title = elem.innerHTML;
+        data.title = elem.innerText;
         data.link = elem.href;
 
         return data;
       });
-      const related = Array.from(document.querySelectorAll('a.li')).map(
-        (elem) => {
-          const data = {};
 
-          data.sourceId = 3;
-          data.title = elem.innerText;
-          data.link = elem.href;
+      const moreNews = Array.from(
+        document.querySelectorAll('div.article-title-h4 > a')
+      ).map((elem) => {
+        const data = {};
 
-          return data;
-        }
-      );
+        data.sourceId = 3;
+        data.title = elem.innerText;
+        data.link = elem.href;
 
-      return [headline, ...latest, ...related];
+        return data;
+      });
+
+      return [headline, ...latest, ...moreNews];
     });
 
-    // Handle Duplicates
-    const news = handleDuplicates(results);
     await browser.close();
-    return news;
+    return results;
   } catch (error) {
     console.error(error);
   }
 };
+// const scrapeAbscbn = async () => {
+//   try {
+//     const chromeOptions = {
+//       headless: true,
+//       defaultViewport: null,
+//       args: [
+//         '--incognito',
+//         '--no-zygote',
+//         '--single-process',
+//         '--no-sandbox',
+//         '--disable-setuid-sandbox',
+//       ],
+//     };
 
-module.exports = scrapeAbscbn;
+//     const url = 'https://news.abs-cbn.com/anc/exclusives';
+//     const browser = await puppeteer.launch(chromeOptions);
+//     const page = await browser.newPage();
+//     await page.setUserAgent(
+//       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+//     );
+
+//     await page.goto(url, {
+//       waitUntil: 'domcontentloaded',
+//       timeout: 0,
+//     });
+
+//     await page.screenshot({ fullPage: true, path: 'screenshot.png' });
+//     // Grab news headlines
+//     const results = await page.evaluate(() => {
+//       const elem = document.querySelector('a.section-top1-title-link');
+//       const headline = {};
+//       headline.sourceId = 3;
+//       headline.title = elem.innerHTML;
+//       headline.link = elem.href;
+
+//       const latest = Array.from(
+//         document.querySelectorAll('a.s-title-small-link')
+//       ).map((elem) => {
+//         const data = {};
+
+//         data.sourceId = 3;
+//         data.title = elem.innerHTML;
+//         data.link = elem.href;
+
+//         return data;
+//       });
+//       const related = Array.from(document.querySelectorAll('a.li')).map(
+//         (elem) => {
+//           const data = {};
+
+//           data.sourceId = 3;
+//           data.title = elem.innerText;
+//           data.link = elem.href;
+
+//           return data;
+//         }
+//       );
+
+//       return [headline, ...latest, ...related];
+//     });
+
+//     // Handle Duplicates
+//     const news = handleDuplicates(results);
+//     await browser.close();
+//     return news;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+module.exports = scrapeManilaTimes;
--- server/lib/scrapers/scrapeAllArticles.js
+++ server/lib/scrapers/scrapeAllArticles.js
@@ -1,16 +1,16 @@
 const scrapeRappler = require('./rappler');
-const scrapeAbscbn = require('./abscbn');
+const scrapeManilaTimes = require('./abscbn');
 const scrapePhilstar = require('./philstar');
 const scrapeManilaBul = require('./manilaBulletin');
 
 const scrapeAllArticles = async () => {
   try {
     const rappler = await scrapeRappler();
-    const abscbn = await scrapeAbscbn();
+    const manilaTimes = await scrapeManilaTimes();
     const philstar = await scrapePhilstar();
     const manilaBulletin = await scrapeManilaBul();
 
-    return [...rappler, ...abscbn, ...philstar, ...manilaBulletin];
+    return [...rappler, ...manilaTimes, ...philstar, ...manilaBulletin];
   } catch (error) {
     console.error(error);
   }
